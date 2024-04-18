bcc263e6c97dff16a9a50ad31275b21d6aeac8a0
Shikha Dhiman <dshikha4403@gmail.com>
2024-04-18 21:59:45 +0530

    modified insertion sort

--- Sorting/insertionSort.cpp
+++ Sorting/insertionSort.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-void insertSort(vector<int> arr){
+void insertSort(vector<int> &arr){
     for(int i = 1; i < arr.size(); i++){
         int key = arr[i];
         int j = i-1;
@@ -19,17 +19,40 @@ void insertSort(vector<int> arr){
 }
 
 void insertSort2(vector<int> &arr){
-    for(int i = 1; i < arr.size(); i++){
+    for(int i = 0; i < arr.size(); i++){
+
         int j = i;
 
-        while(j-- && arr[j-1] > arr[j]){
+        while(j>0 && arr[j-1] > arr[j]){
             int temp = arr[j-1];
             arr[j-1] = arr[j];
-            arr[j]
+            arr[j] = temp;
+
+            j--;
         }
     }
+    return;
 }
 
 int main(){
+    vector<int> nums = {9, 3, 19, 0, 5, 12, 1, 0};
+
+    insertSort(nums);
+
+    cout<<"\n";
+    for(auto x : nums){
+        cout<<x<<" ";
+    }
+    cout<<"\n\n";
+    
+    vector<int> nums2 = {10, 5, 7, 2, 1, 9, 0, 3};
+    insertSort2(nums2);
+
+    cout<<"\n";
+    for(auto x : nums2){
+        cout<<x<<" ";
+    }
+    cout<<"\n\n";
+
     return 0;
 }

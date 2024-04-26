f58fea5c72dafd3635c438d98aa9545dea80f62d
Shikha Dhiman <dshikha4403@gmail.com>
2024-04-26 20:53:14 +0530

    Learning more about arrays

---
+++ Arrays/majorityElement.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+int majorElement(vector<int> &nums){
+    unordered_map<int, int> mp;
+    int n = nums.size();
+    for(int i = 0; i < n; i++){
+        mp[nums[i]]++;
+        if(mp[nums[i]] > n/2) return nums[i];
+    }
+    return -1;
+}
+
+int majorElement2(vector<int> nums){
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    return nums[n/2];
+}
+
+// Moore's 
+int majorElement3(vector<int> &nums){
+    int count = 0;
+    int candidate = nums[0];
+
+    for(int i = 0; i < nums.size(); i++){
+        if(count == 0){
+            candidate = nums[i];
+        }
+
+        if(nums[i] == candidate){
+            count++;
+        }else{
+            count--;
+        }
+    }
+
+    return candidate;
+}
+
+int main(){
+    vector<int> arr = {3, 2, 3, 9, 1, 3, 0, 3, 3};
+    cout<<majorElement(arr)<<"\n";
+    cout<<majorElement2(arr)<<"\n";
+    cout<<majorElement3(arr)<<"\n";
+
+    return 0;
+}
---
+++ Arrays/maxSubarraySum.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxSubarraySum(vector<int> &nums){
+    // Brute Force :---------------------------->>>>>>>>>>>
+    // int maxSum = nums[0];
+    // for(int i = 0; i < nums.size(); i++){
+    //     int sum = 0;
+    //     for(int j = i; j < nums.size(); j++){
+    //         sum += nums[j];
+    //         maxSum = max(maxSum, sum);
+    //     }
+    // }
+    // return maxSum;
+
+    // Optimal Approach:------------------>>>>>>>>
+    // KADANE's algorithm
+    int sum = 0;
+    int maxSum = nums[0];
+
+    for(int i = 0; i < nums.size(); i++){
+        sum += nums[i];
+        maxSum = max(maxSum, sum);
+        if(sum < 0) sum = 0;
+    }
+    return maxSum;
+
+    // To get the index of the subarray:------->>>>
+    // int sum = 0;
+    // int maxSum = nums[0];
+    // int start = 0;
+    // int end = 0;
+    // for(int i = 0; i < nums.size(); i++){
+    //     sum += nums[i];
+    //     if(sum > maxSum || sum == maxSum){
+    //         maxSum = sum;
+    //         end = i;
+    //     }
+    //     if(sum < 0){
+    //         sum = 0;
+    //         start++;
+    //     }
+    // }
+    // cout<<start<<", "<<end<<"\n";
+    // return maxSum;
+}
+
+int main(){
+    vector<int> arr = {-1, -2, 3, 10, -4, 6, -2, 8, 0};
+    cout<<maxSubarraySum(arr)<<"\n";
+    return 0;
+}

f2925c117a2361d9d28e722f15c6d4036bc2782a
Shikha Dhiman <dshikha4403@gmail.com>
2024-07-04 23:56:31 +0530

    more on stacks

--- Stack/largestRectangleInHistogram.cpp
+++ Stack/largestRectangleInHistogram.cpp
@@ -64,10 +64,29 @@ int largestRectangleArea2(vector<int> &heights){
 
     return ans;
 }
+
+int largestRectangleArea3(vector<int> &heights){
+    stack<int> st;
+    int n = heights.size();
+    int maxArea = 0;
+    for(int i = 0; i <= n; i++){
+        while(!st.empty() && (i == n || heights[st.top()] > heights[i])){
+            int height = heights[st.top()];
+            st.pop();
+            int width;
+            if(st.empty()) width = i;
+            else width = i - st.top() - 1;
+            maxArea = max(maxArea, width * height);
+        }
+        st.push(i);
+    }
+
+    return maxArea;
+}
 int main(){
     vector<int> heights = {2,1,5,6,2,3,1};
     cout<<largestRectangleArea(heights)<<"\n";
     cout<<largestRectangleArea2(heights)<<"\n";
-
+    cout<<largestRectangleArea3(heights)<<"\n";
     return 0;
 }
